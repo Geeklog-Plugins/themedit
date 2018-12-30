0a20d9d65bd5885a96c07869478c00f675ae3207
Kenji ITO <mystralkk@gmail.com>
2018-12-30 15:10:55 +0900

    Imported v1.0.4 for Geeklog-1.4.0

--- admin/index.php
+++ admin/index.php
@@ -31,17 +31,17 @@
 //
 // $Id$
 
-require_once('../../../lib-common.php');
+require_once '../../../lib-common.php';
 
 // Only let admin users access this page
-if (!SEC_hasRights('themedit.admin')) {
+if ( !SEC_hasRights( 'themedit.admin' ) ) {
     // Someone is trying to illegally access this page
-    COM_errorLog("Someone has tried to illegally access the themedit Admin page.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1);
+    COM_errorLog( "Someone has tried to illegally access the themedit Admin page.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: $REMOTE_ADDR",1 );
     $display  = COM_siteHeader();
-    $display .= COM_startBlock($LANG_THM['access_denied']);
+    $display .= COM_startBlock( $LANG_THM['access_denied'] );
     $display .= $LANG_THM['access_denied_msg'];
     $display .= COM_endBlock();
-    $display .= COM_siteFooter(true);
+    $display .= COM_siteFooter( true );
     echo $display;
     exit;
 }
@@ -58,18 +58,18 @@ $contents = '';
 
 // Checks if themes and/or files are added and/or deleted
 
-switch (strtolower($_THM_CONF['resync_database'])) {
+switch ( strtolower( $_THM_CONF['resync_database'] ) ) {
 	case 'auto':
 		$diff = THM_isAddedOrRemoved();
-		if (count($diff['added']) > 0 || count($diff['removed']) > 0) {
+		if ( count( $diff['added'] ) > 0 || count( $diff['removed'] ) > 0 ) {
 			THM_updateAll();
 		}
 		break;
 	case 'manual':
 		$diff = THM_isAddedOrRemoved();
-		if (count($diff['added']) > 0 || count($diff['removed']) > 0) {
+		if ( count( $diff['added'] ) > 0 || count( $diff['removed'] ) > 0 ) {
 			$link = $_CONF['site_admin_url'] . '/plugins/themedit/index.php?op=updateall';
-			$sys_message .= str_replace('%s', $link, $LANG_THM['file_changed']);
+			$sys_message .= str_replace( '%s', $link, $LANG_THM['file_changed'] );
 		}
 		break;
 	case 'ignore':
@@ -83,212 +83,216 @@ $theme = $theme_names[0];
 
 // Undo magic_quotes if necessary
 
-if (get_magic_quotes_gpc() == 1) {
-	$_GET  = array_map('stripslashes', $_GET);
-	$_POST = array_map('stripslashes', $_POST);
+if ( get_magic_quotes_gpc() == 1 ) {
+	$_GET  = array_map( 'stripslashes', $_GET );
+	$_POST = array_map( 'stripslashes', $_POST );
 }
-if (isset($_POST['thm_theme'])) {
-	$req_theme = COM_applyFilter($_POST['thm_theme']);
-} else if (isset($_GET['thm_theme'])) {
-	$req_theme = COM_applyFilter($_GET['thm_theme']);
+if ( isset( $_POST['thm_theme'] ) ) {
+	$req_theme = COM_applyFilter( $_POST['thm_theme'] );
+} else if ( isset( $_GET['thm_theme'] ) ) {
+	$req_theme = COM_applyFilter( $_GET['thm_theme'] );
 }
-if (in_array($req_theme, $theme_names)) {
+if ( in_array( $req_theme, $theme_names ) ) {
 	$theme = $req_theme;
 }
 
-if (isset($_POST['thm_file'])) {
-	$req_file = COM_applyFilter($_POST['thm_file']);
-} else if (isset($_GET['thm_file'])) {
-	$req_file = COM_applyFilter($_GET['thm_file']);
+if ( isset( $_POST['thm_file'] ) ) {
+	$req_file = COM_applyFilter( $_POST['thm_file'] );
+} else if ( isset( $_GET['thm_file'] ) ) {
+	$req_file = COM_applyFilter( $_GET['thm_file'] );
 }
-if (in_array($req_file, $_THM_CONF['allowed_files'])) {
+if ( in_array( $req_file, $_THM_CONF['allowed_files'] ) ) {
 	$file = $req_file;
 }
 
-if (isset($_POST['thm_op'])) {
-	$op = COM_applyFilter($_POST['thm_op']);
-} else if (isset($_GET['op'])) {
-	$op = COM_applyFilter($_GET['op']);
+if ( isset( $_POST['thm_op'] ) ) {
+	$op = COM_applyFilter( $_POST['thm_op'] );
+} else if ( isset( $_GET['op'] ) ) {
+	$op = COM_applyFilter( $_GET['op'] );
 }
-if ($op == '' && $file != '') {
+if ( $op == '' && $file != '' ) {
 	$op = 'load';
 }
 
-if (isset($_POST['theme_contents'])) {
+if ( isset( $_POST['theme_contents'] ) ) {
 	$contents = $_POST['theme_contents'];
 }
 
 // Checks if $file is writable
 
-if (! empty($file)) {
-	if (! THM_isWritable($theme, $file)) {
+if ( ! empty( $file ) ) {
+	if ( ! THM_isWritable( $theme, $file ) ) {
 		$sys_message .= $LANG_THM['not_writable'];
 	}
 }
 
 // Operation
 
-if ($op == $LANG_THM['save']) {
-	$result = THM_saveFile($theme, $file, $contents);
-	if ($result) {
+if ( $op == $LANG_THM['save'] ) {
+	$result = THM_saveFile( $theme, $file, $contents );
+	if ( $result ) {
 		$sys_message = $LANG_THM['save_success'];
 	} else {
 		$sys_message = $LANG_THM['save_fail'];
 	}
-} else if ($op == $LANG_THM['init']) {
-	$result = THM_initFile($theme, $file);
-	if ($result) {
-		$contents = THM_getContents($theme, $file);
+} else if ( $op == $LANG_THM['init'] ) {
+	$result = THM_initFile( $theme, $file );
+	if ( $result ) {
+		$contents = THM_getContents( $theme, $file );
 		$sys_message = $LANG_THM['init_success'];
 	} else {
 		$sys_message = $LANG_THM['init_fail'];
 	}
-} else if ($op == 'updateall') {
+} else if ( $op == 'updateall' ) {
 	THM_updateAll();
+} else if ( $op == $LANG_THM['image'] ) {
+	header( 'Location: ' . $_CONF['site_admin_url'] . '/plugins/themedit/upload.php' );
+	exit;
 }
 
 // Display
 
 $display = COM_siteHeader();
-$T = new Template($_CONF['path'] . 'plugins/themedit/templates');
-$T->set_file('admin', 'admin.thtml');
+$T = new Template( $_CONF['path'] . 'plugins/themedit/templates' );
+$T->set_file( 'admin', 'admin.thtml' );
 // To prevent template engine from removing template vars loaded in <textarea>
-$T->set_unknowns('keep');
-$T->set_var('temp_site_url', $_CONF['site_url']);
-$T->set_var('temp_site_admin_url', $_CONF['site_admin_url']);
-$T->set_var('temp_header', $LANG_THM['admin']);
+$T->set_unknowns( 'keep' );
+$T->set_var( 'temp_site_url', $_CONF['site_url'] );
+$T->set_var( 'temp_site_admin_url', $_CONF['site_admin_url'] );
+$T->set_var( 'temp_header', $LANG_THM['admin'] );
 $code4preview = <<<EOD1
 <script type="text/javascript">
 <!--
-	window.open("{$_CONF['site_admin_url']}/plugins/themedit/preview.html", "PREVIEW");
+	window.open( "{$_CONF['site_admin_url']}/plugins/themedit/preview.html", "PREVIEW" );
 //-->
 </script>
 EOD1;
 
-if ($op == $LANG_THM['preview']) {
+if ( $op == $LANG_THM['preview'] ) {
 	// If a file is being edited, first swap its contents with that of the
 	// corresponding file saved on the Web, then create a preview, and finally
 	// restore the file contents
-	if (! empty($file)) {
-		$path_parts = pathinfo($file);
-		$is_css = (strtolower($path_parts['extension']) == 'css');
-		if ($is_css) {
-			$fh = fopen($_CONF['path_html'] . 'admin/plugins/themedit/preview.css', 'wb');
-			if ($fh !== false) {
-				fwrite($fh, $contents);
-				fclose($fh);
+	if ( ! empty( $file ) ) {
+		$path_parts = pathinfo( $file );
+		$is_css = ( strtolower( $path_parts['extension'] ) == 'css' );
+		if ( $is_css ) {
+			$fh = fopen( $_CONF['path_html'] . 'admin/plugins/themedit/preview.css', 'wb' );
+			if ( $fh !== false ) {
+				fwrite( $fh, $contents );
+				fclose( $fh );
 			}
 		} else {
-			$org_contents = THM_getContents($theme, $file);
-			THM_saveFile($theme, $file, $contents);
+			$org_contents = THM_getContents( $theme, $file );
+			THM_saveFile( $theme, $file, $contents );
 		}
 	}
 	
 	$preview = THM_getPreview();
 	
-	if (! empty($file) && ! $is_css) {
-		THM_saveFile($theme, $file, $org_contents);
+	if ( ! empty( $file ) && ! $is_css ) {
+		THM_saveFile( $theme, $file, $org_contents );
 	}
 	
-	$preview = preg_replace('/(^.*?<title>).*?(<\/title>.*$)/mi', '$1' . $LANG_THM['preview'] . '$2', $preview);
-	list(, $dummy) = explode(' ', microtime());
+	$preview = preg_replace( '/( ^.*?<title> ).*?( <\/title>.*$ )/mi', '$1' . $LANG_THM['preview'] . '$2', $preview );
+	list( , $dummy ) = explode( ' ', microtime() );
 	
 	// To make sure your browser reads a CSS file afresh, not from cache
-	if ($is_css) {
+	if ( $is_css ) {
 		$css_path = $_CONF['site_url'] . '/layout/' . $theme . '/' . $file;
 		$alt_css_path = $_CONF['site_admin_url'] . '/plugins/themedit/preview.css?dummy=' . $dummy;
-		$pos = strpos(strtolower($preview), strtolower($css_path));
-		COM_errorLog('$preview: ' . $preview . "\r\n" . '$css_path: ' . $css_path);
-		if ($pos !== false) {
-			$preview = substr($preview, 0, $pos) . $alt_css_path . substr($preview, $pos + strlen($css_path));
+		$pos = strpos( strtolower( $preview ), strtolower( $css_path ) );
+		COM_errorLog( '$preview: ' . $preview . "\r\n" . '$css_path: ' . $css_path );
+		if ( $pos !== false ) {
+			$preview = substr( $preview, 0, $pos ) . $alt_css_path . substr( $preview, $pos + strlen( $css_path ) );
 		}
-//		$preview = str_replace($css_path, $alt_css_path, $preview);
+//		$preview = str_replace( $css_path, $alt_css_path, $preview );
 	} else {
-		$preview = preg_replace(
-			'/(^.*?)(href=".*\.css)(".*$)/im',
+		$preview = preg_replace( 
+			'/( ^.*? )( href=".*\.css )( ".*$ )/im',
 			'$1$2?dummy=' . $dummy . '$3',
 			$preview
-		);
+		 );
 	}
-	$fh = fopen($_CONF['path_html'] . 'admin/plugins/themedit/preview.html', 'wb');
-	if ($fh !== false) {
-		fwrite($fh, $preview);
-		fclose($fh);
+	$fh = fopen( $_CONF['path_html'] . 'admin/plugins/themedit/preview.html', 'wb' );
+	if ( $fh !== false ) {
+		fwrite( $fh, $preview );
+		fclose( $fh );
 	}
-	$T->set_var('temp_preview_code', $code4preview);
+	$T->set_var( 'temp_preview_code', $code4preview );
 } else {
-	$T->set_var('temp_preview_code', '');
+	$T->set_var( 'temp_preview_code', '' );
 }
 
-if (empty($sys_message)) {
-	$T->set_var('temp_sys_message', '');
+if ( empty( $sys_message ) ) {
+	$T->set_var( 'temp_sys_message', '' );
 } else {
-	$T->set_var(
+	$T->set_var( 
 		'temp_sys_message',
 		'<p style="border: solid 2px red; padding: 5px;">' . $sys_message . '</p>'
-	);
+	 );
 }
-$T->set_var('temp_lang_script_disabled', '<p style="color: red; font-weight: bold;">' . $LANG_THM['script_disabled'] . '</p>');
-$T->set_var('temp_lang_select', $LANG_THM['select']);
-$T->set_var('temp_lang_theme_edited', $LANG_THM['theme_edited']);
-$T->set_var('temp_lang_file_edited', $LANG_THM['file_edited']);
+$T->set_var( 'temp_lang_script_disabled', '<p style="color: red; font-weight: bold;">' . $LANG_THM['script_disabled'] . '</p>' );
+$T->set_var( 'temp_lang_select', $LANG_THM['select'] );
+$T->set_var( 'temp_lang_theme_edited', $LANG_THM['theme_edited'] );
+$T->set_var( 'temp_lang_file_edited', $LANG_THM['file_edited'] );
 
 // Set theme name drop down list
 
 $themes4html = '';
-foreach ($theme_names as $theme_name) {
-	if ($theme_name == $theme) {
-		$themes4html .= '<option selected="selected">' . $theme_name;
+foreach ( $theme_names as $theme_name ) {
+	if ( $theme_name == $theme ) {
+		$themes4html .= "<option value='{$theme_name}' selected='selected'>";
 	} else {
-		$themes4html .= '<option>' . $theme_name;
+		$themes4html .= "<option value='{$theme_name}'>";
 	}
-	$themes4html .= '</option>' . LB;
+	$themes4html .= htmlspecialchars(  $theme_name, ENT_QUOTES  ) . '</option>' . LB;
 }
-$T->set_var('temp_themes', $themes4html);
+$T->set_var( 'temp_themes', $themes4html );
 
 // Set template/css name drop down list
 
-if ($file == '') {
+if ( $file == '' ) {
 	$files4html = '<option selected="selected">';
 } else {
 	$files4html = '<option>';
 }
 $files4html .= '-</option>' . LB;
-foreach ($_THM_CONF['allowed_files'] as $allowed_file) {
-	if ($allowed_file == $file) {
-		$files4html .= '<option selected="selected">';
+foreach ( $_THM_CONF['allowed_files'] as $allowed_file ) {
+	if ( $allowed_file == $file ) {
+		$files4html .= "<option value='{$allowed_file}' selected='selected'>";
 	} else {
-		$files4html .= '<option>';
+		$files4html .= "<option value='{$allowed_file}'>";
 	}
-	$files4html .= $allowed_file . '</option>' . LB;
+	$files4html .= htmlspecialchars(  $LANG_THM[$allowed_file], ENT_QUOTES  )
+			    . '</option>' . LB;
 }
-$T->set_var('temp_files', $files4html);
+$T->set_var( 'temp_files', $files4html );
 
 // Load template vars & file contents
 
-if (! empty($file)) {
-	$vars = THM_getTemplateVars($theme, $file);
+if ( ! empty( $file ) ) {
+	$vars = THM_getTemplateVars( $theme, $file );
 }
-if ($op == 'load') {
-	$contents = THM_getContents($theme, $file);
+if ( $op == 'load' ) {
+	$contents = THM_getContents( $theme, $file );
 }
-$contents4html = htmlspecialchars($contents, ENT_QUOTES, $_CONF['default_charset']);
+$contents4html = htmlspecialchars( $contents, ENT_QUOTES, $_CONF['default_charset'] );
 
 // In case of a template file, show a list of vars available
 
 $vars4html = '';
-if (count($vars) > 0) {
+if ( count( $vars ) > 0 ) {
 	$vars4html .= '<table style="border: solid 1px #7F9DB9; padding: 5px; width: 100%">';
 	$vars4html .= '<caption style="text-align: center; color: white; background-color: #7F9DB9;">';
 	$vars4html .= $LANG_THM['vars_available'] . '</caption>';
 	$vars4html .= '<tr>';
-	for ($i = 0, $j = 0; $i < count($vars); $i ++) {
+	for ( $i = 0, $j = 0; $i < count( $vars ); $i ++ ) {
 		$vars4html .= '<td width="150"><button type="button" title="';
-		$vars4html .= $LANG_THM["help_{$vars[$i]}"] . '" onClick="insert_var(\'' . $vars[$i]. '\')"';
+		$vars4html .= $LANG_THM["help_{$vars[$i]}"] . '" onClick="insert_var( \'' . $vars[$i]. '\' )"';
 		$vars4html .= ' style="color: white; background-color: #333366;">';
 		$vars4html .= $vars[$i] . '</button></td>';
 		$j ++;
-		if ($j % 4 == 0) {
+		if ( $j % 4 == 0 ) {
 			$vars4html .= '</tr>' . LB . '<tr>';
 		}
 	}
@@ -297,14 +301,15 @@ if (count($vars) > 0) {
 	$vars4html .= '</table>' . LB;
 }
 
-$T->set_var('temp_vars', $vars4html);
-$T->set_var('temp_contents', $contents4html);
-$T->set_var('temp_lang_preview', $LANG_THM['preview']);
-$T->set_var('temp_lang_save', $LANG_THM['save']);
-$T->set_var('temp_lang_init', $LANG_THM['init']);
+$T->set_var( 'temp_vars', $vars4html );
+$T->set_var( 'temp_contents', $contents4html );
+$T->set_var( 'temp_lang_preview', $LANG_THM['preview'] );
+$T->set_var( 'temp_lang_save', $LANG_THM['save'] );
+$T->set_var( 'temp_lang_image', $LANG_THM['image'] );
+$T->set_var( 'temp_lang_init', $LANG_THM['init'] );
 
-$T->parse('output','admin');
-$display .= $T->finish($T->get_var('output'));
+$T->parse( 'output','admin' );
+$display .= $T->finish( $T->get_var( 'output' ) );
 $display .= COM_siteFooter();
 
 echo $display;
--- config.php
+++ config.php
@@ -47,9 +47,17 @@ $_THM_CONF = array();
 
 // Plugin info
 
-$_THM_CONF['pi_version'] = '1.0.3';						// Plugin Version
+$_THM_CONF['pi_version'] = '1.0.4';						// Plugin Version
 $_THM_CONF['gl_version'] = '1.4.0';						// GL Version plugin for
 $_THM_CONF['pi_url']     = 'http://mystral-kk.net/';	// Plugin Homepage
+
+//=========================================================
+//  USER CONFIGURATION
+//=========================================================
+
+//-------------------------------------
+//  Theme names and file names
+//-------------------------------------
 
 // Themes to be edited with this plugin
 // NOTE: theme names are case-sensitive
@@ -62,34 +70,29 @@ $_THM_CONF['allowed_themes'] = array(
 // NOTE: file names are case-sensitive
 
 $_THM_CONF['allowed_files'] = array(
-	'header.thtml', 'footer.thtml', 'leftblocks.thtml', 'rightblocks.thtml',
-	'blockheader-left.thtml', 'blockfooter-left.thtml',
-	'blockheader-right.thtml', 'blockfooter-right.thtml',
-	'featuredstorytext.thtml', 'storytext.thtml','archivestorytext.thtml',
-	'menuitem.thtml', 'menuitem_last.thtml', 'menuitem_none.thtml',
-	'list.thtml','listitem.thtml',
-	'loginform.thtml',
 	'style.css',
+	'header.thtml', 'footer.thtml',
+	'leftblocks.thtml', 'blockheader-left.thtml', 'blockfooter-left.thtml',
+	'rightblocks.thtml', 'blockheader-right.thtml', 'blockfooter-right.thtml',
+	'storytext.thtml', 'featuredstorytext.thtml', 'archivestorytext.thtml',
 	'article/article.thtml', 'article/printable.thtml',
+	'menuitem.thtml', 'menuitem_last.thtml', 'menuitem_none.thtml',
+	'list.thtml', 'listitem.thtml',
+	'loginform.thtml',
 	'profiles/contactuserform.thtml', 'profiles/contactauthorform.thtml',
-	'preferences/profile.thtml',
+	'preferences/profile.thtml', 'users/profile.thtml',
 	'search/searchform.thtml', 'search/searchresults.thtml',
 	'submit/submitevent.thtml', 'submit/submitloginrequired.thtml',
-	'users/newpassword.thtml', 'users/commentrow.thtml',
-	'users/getpasswordform.thtml', 'users/loginform.thtml', 'users/profile.thtml',
-	'users/registrationform.thtml', 'users/storyrow.thtml'
+	'users/newpassword.thtml', 'users/getpasswordform.thtml',
+	'users/loginform.thtml', 'users/registrationform.thtml',
+	'users/storyrow.thtml', 'users/commentrow.thtml', 
 );
 
-// Additional themes and files for Geeklog.jp users
+// If you'd like to see theme names and file names sorted alphabetically in
+// their dropdown list, uncomment the next two lines.
 
-if (isset($_CONF['language'])
- && in_array($_CONF['language'], array('japanese', 'japanese_utf-8'))) {
-	$_THM_CONF['allowed_themes'][] = 'IvySOHO';
-	$_THM_CONF['allowed_files'][]  = 'style_jp.css';
-	$_THM_CONF['allowed_files'][]  = 'style_forum.css';
-	sort($_THM_CONF['allowed_themes']);
-	sort($_THM_CONF['allowed_files']);
-}
+// sort($_THM_CONF['allowed_themes']);
+// sort($_THM_CONF['allowed_files']);
 
 // When you add/remove a theme to/from $_THM_CONF['allowed_themes'], or a template
 // file to/from $_THM_CONF['allowed_files'], Theme Editor plugin will detect it
@@ -99,5 +102,45 @@ if (isset($_CONF['language'])
 // will do nothing about the change.
 
 $_THM_CONF['resync_database'] = 'manual';
+
+//-------------------------------------
+//  Image upload
+//-------------------------------------
+
+// If set true, you can upload images to theme/images/* directories
+
+$_THM_CONF['allow_upload'] = true;
+
+// Thumbnail size
+
+$_THM_CONF['image_width']  = 120;	// In pixels
+
+$_THM_CONF['image_height'] = 100;	// In pixels
+
+// Max column number of thumbnails
+
+$_THM_CONF['image_max_col'] = 6;
+
+// Max size of a file for uploading to the web server
+
+$_THM_CONF['upload_max_size'] = 1048576;	// In bytes (1048576 bytes = 1M bytes)
+
+//=========================================================
+//  END OF USER CONFIGURATION
+//=========================================================
+
+// You won't have to change anything below this line
+
+// Additional themes and files for Geeklog.jp users
+
+if (isset($_CONF['language'])
+ && in_array($_CONF['language'], array('japanese', 'japanese_utf-8'))) {
+	$_THM_CONF['allowed_themes'][] = 'IvySOHO';
+	$_THM_CONF['allowed_themes'][] = 'Geek_and_Loggloo';
+	$_THM_CONF['allowed_themes'][] = 'mobile';
+	$_THM_CONF['allowed_themes'][] = 'ProfessionalCSS';
+	$_THM_CONF['allowed_files'][]  = 'style_jp.css';
+	$_THM_CONF['allowed_files'][]  = 'style_forum.css';
+}
 
 ?>
